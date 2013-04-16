2085a4883aecb0c51e97f450733c88db5698d48d
Loris Tissino <loris.tissino@gmail.com>
2013-04-16 08:06:30 +0200

    Fix: edited journal view for better rendering of debited accounts

--- protected/views/bookkeeping/_account.php
+++ protected/views/bookkeeping/_account.php
@@ -1,8 +1,8 @@
-<span class="j<?php echo $amount>0 ? 'debit':'credit' ?>"><?php echo CHtml::link($account->name, $this->createUrl('bookkeeping/ledger', array('id'=>$account->id, 'post'=>$post->id)), array('class'=>'hiddenlink')) ?>
+<div class="j<?php echo $amount>0 ? 'debit':'credit' ?>"><?php echo CHtml::link($account->name, $this->createUrl('bookkeeping/ledger', array('id'=>$account->id, 'post'=>$post->id)), array('class'=>'hiddenlink')) ?>
 <?php if(!$account->is_selectable): ?>
   <span class="warning" title="<?php echo Yii::t('delt', 'This account now has children, so it should not be used directly anymore.') ?>">
   <?php echo CHtml::image(Yii::app()->request->baseUrl.'/images/bell.png') ?>
   </span>
 <?php endif ?>
-</span>
+</div>
 
