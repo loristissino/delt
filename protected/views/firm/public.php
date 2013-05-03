da77efd7351cdc042daec751325ba8fe294b9c1f
Loris Tissino <loris.tissino@gmail.com>
2013-05-03 17:22:53 +0200

    Enh: improved public view

--- protected/views/firm/public.php
+++ protected/views/firm/public.php
@@ -9,7 +9,7 @@ $this->breadcrumbs=array(
   'Public',
 );
 */
-$debitcredits = $this->firm->getPostsAsDataProvider()->data;
+$debitcredits = $this->firm->getPostsAsDataProvider(100000)->data;
 
 ?>
 
@@ -18,7 +18,7 @@ $debitcredits = $this->firm->getPostsAsDataProvider()->data;
 <section>
 <h2><?php echo Yii::t('delt', 'Journal') ?></h2>
 
-<p>This is only a proof of concept. This page will contain all public information available about this firm.</p>
+<p>This is just a proof of concept, for now. This page will contain all public information available about this firm.</p>
 
 <table>
   <tr>
