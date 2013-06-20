6ecca593b1f6eb62ee1e9b0ed7982175ea084ba4
Loris Tissino <loris.tissino@gmail.com>
2013-06-20 18:44:24 +0200

    Enh: clean URLs for firms/*

--- protected/views/bookkeeping/manage.php
+++ protected/views/bookkeeping/manage.php
@@ -17,7 +17,7 @@ $this->menu=array(
     )),
 	array('label'=>Yii::t('delt', 'Export'), 'url'=>array('/bookkeeping/export', 'slug'=>$model->slug)),
 	array('label'=>Yii::t('delt', 'Import'), 'url'=>array('/bookkeeping/import', 'slug'=>$model->slug)),
-	array('label'=>Yii::t('delt', 'Show'), 'url'=>array('/firm/'.$model->slug)),
+	array('label'=>Yii::t('delt', 'Show'), 'url'=>array('/firms/'.$model->slug)),
 	array('label'=>Yii::t('delt', 'Share'), 'url'=>array('/firm/share', 'slug'=>$model->slug)),
 );
 
