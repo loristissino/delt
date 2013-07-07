086414711ad22ea77b2927495014a5a6aeb0cb16
Loris Tissino <loris.tissino@gmail.com>
2013-07-07 16:20:58 +0200

    Enh: changed bottom line

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -59,7 +59,7 @@ $mainmenu_items=array(
 	<div class="clear"></div>
 
 	<div id="footer">
-		Website based on <a href="http://loristissino.github.com/delt/">DELT</a>, Double Entry Learning Tool (release <?php echo DELT::getVersion() ?>) - Copyright &copy; <?php echo date('Y'); ?> by Loris Tissino. All Rights Reserved.<br/>
+		Website based on <a href="http://loristissino.github.com/delt/">DELT</a>, Double Entry Learning Tool (release <?php echo DELT::getVersion() ?>) - Copyright &copy; <?php echo date('Y'); ?> by DELT Project. All Rights Reserved.<br/>
 		<?php echo Yii::powered(); ?>
     <?php echo Yii::app()->params['tagLine'] ?>
 	</div><!-- footer -->
