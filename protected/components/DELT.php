e79aced2bba86bd9eb90c68155e2b095875051ae
Loris Tissino <loris.tissino@gmail.com>
2013-08-11 20:59:39 +0200

    Ver 1.0.12

--- protected/components/DELT.php
+++ protected/components/DELT.php
@@ -5,7 +5,7 @@ class DELT
   
   public static function getVersion()
   {
-    return '1.0.11';
+    return '1.0.12';
   }
   
   public static function currency_value($amount, $currency, $with_debit_credit=false, $with_zero=false, $element='', $htmlOptions=array())
