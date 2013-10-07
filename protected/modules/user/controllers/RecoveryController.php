9e15055c44a37014f3262fe0d66a7d2a08bd4c35
Loris Tissino <loris.tissino@gmail.com>
2013-10-07 17:42:33 +0200

    Fix: resend activation link for validated users

--- protected/modules/user/controllers/RecoveryController.php
+++ protected/modules/user/controllers/RecoveryController.php
@@ -77,7 +77,7 @@ class RecoveryController extends Controller
 			    		if($form->validate()) {
 			    			$user = User::model()->notsafe()->findbyPk($form->user_id);
                 
-                if($user->status!=User::STATUS_NOACTIVE)
+                if($user->status!=User::STATUS_NOACTIVE or $user->status!=User::STATUS_WAITING)
                 {
                   Yii::app()->user->setFlash('resendMessage',UserModule::t("Your account is not waiting for activation."));
                   $this->refresh();
