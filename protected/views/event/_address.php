52a677829d36574641d4c0d5a76a3c432202c22b
Loris Tissino <loris.tissino@gmail.com>
2014-03-03 18:07:28 +0100

    Fix: added reference to external service

--- protected/views/event/_address.php
+++ protected/views/event/_address.php
@@ -1,3 +1,3 @@
 <?php if($event->address != '127.0.0.1'): ?>
-  <?php echo CHtml::link($event->address, Yii::app()->params['iplocator'], array('target'=>'_blank')) ?>
+  <?php echo CHtml::link($event->address, str_replace('%ip%', $event->address, Yii::app()->params['iplocator']), array('target'=>'_blank')) ?>
 <?php endif ?>
