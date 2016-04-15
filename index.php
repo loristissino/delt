beb4b21c119a905c50990a7b087d9a0a0171c761
Loris Tissino <loris.tissino@gmail.com>
2016-04-15 17:18:46 +0200

    Enh: added missing translations

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
 
+// Use HTTP Strict Transport Security to force client to use secure connections only
+$use_sts = false;
+
+// iis sets HTTPS to 'off' for non-SSL requests
+if ($use_sts && isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') {
+    header('Strict-Transport-Security: max-age=500; includeSubDomains; preload');
+} elseif ($use_sts) {
+    header('Location: https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'], true, 301);
+    // we are in cleartext at the moment, prevent further execution and output
+    die();
+}
+
 // change the following paths if necessary
 $yii=dirname(__FILE__).'/../YiiRoot/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
