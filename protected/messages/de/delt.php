62f6748a411467e446612c6c7f42ecedfc9478b6
Loris Tissino <loris.tissino@gmail.com>
2017-01-17 18:11:15 +0100

    Enh: German l10n began

---
+++ protected/messages/de/delt.php
@@ -0,0 +1,48 @@
+<?php
+  // generated from the POEditor.com file
+
+  return array(
+
+  'Welcome to <i>{name}</i>' => 
+  'Willkommen in <i>{name}</i>',
+
+  'You have no firms that you can use to gain experience in bookkeeping/accounting with.' => 
+  'Man hat keine Firma, mit der man sich ueben kann.',
+
+  'Or, even better,' => 
+  'Oder, noch besser,',
+
+  'Bookkeeping/Accounting' => 
+  'Buchhaltung/Abrechnung',
+
+  'Bookkeeping and Accounting' => 
+  'Buchhaltung und Abrechnung',
+
+  'General Ledger' => 
+  'Hauptbuch',
+
+  'Firm' => 
+  'Firma',
+
+  'Ledger' => 
+  'Hauptbuch',
+
+  'Ledger for account «{name}»' => 
+  'Hauptbuch für Konto «{name}»',
+
+  'Parent account' => 
+  'Elterns account',
+
+  'Description' => 
+  'Beschreibung',
+
+  'Description / Explanation' => 
+  'Beschreibung / Erklärung',
+
+  'Welcome to «{name}»' => 
+  'Willkommen in «{name}»',
+
+  'Fields with <span class="required">*</span> are required.' => 
+  'Felder mit <span class="required">*</ span> sind erforderlich.',
+
+  );
