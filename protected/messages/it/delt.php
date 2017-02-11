ce863800e77d508f586875326a5e48f4eb711ca1
Loris Tissino <loris.tissino@gmail.com>
2017-02-11 09:22:04 +0100

    Fix: typo

--- protected/messages/it/delt.php
+++ protected/messages/it/delt.php
@@ -1489,7 +1489,7 @@
   'Mostra i valori attesi al completamento della sfida',
 
   'Users are allowed to delete the challenge'=>
-  'Agli utenti è permessa l\eliminazione della sfida',
+  'Agli utenti è permessa l\'eliminazione della sfida',
 
   'Event Date'=>
   'Data dell\'evento',
