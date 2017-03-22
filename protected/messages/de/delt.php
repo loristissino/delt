04e8aaca04c89ff8f541589cdb81c677500cc368
Loris Tissino <loris.tissino@gmail.com>
2017-03-22 09:29:54 +0100

    Enh: added some translations (de)

--- protected/messages/de/delt.php
+++ protected/messages/de/delt.php
@@ -7,7 +7,7 @@
   'Willkommen in <i>{name}</i>',
 
   'You have no firms that you can use to gain experience in bookkeeping/accounting with.' => 
-  'Man hat keine Firma, mit der man sich ueben kann.',
+  'Man hat keine Firma, mit der man sich üben kann.',
 
   'Or, even better,' => 
   'Oder, noch besser,',
@@ -18,20 +18,41 @@
   'Bookkeeping and Accounting' => 
   'Buchhaltung und Abrechnung',
 
+  'Journal' => 
+  'Die Zeitschrift',
+
+  'Chart of accounts' => 
+  'Der Kontenplan',
+
   'General Ledger' => 
   'Hauptbuch',
 
+  'Trial Balance' => 
+  'Vorläufige Bilanz',
+
   'Firm' => 
-  'Firma',
+  'Die Firma',
 
   'Ledger' => 
   'Hauptbuch',
 
   'Ledger for account «{name}»' => 
-  'Hauptbuch für Konto «{name}»',
+  'Kontohauptbuch «{name}»',
+
+  'Statements' => 
+  'Die Buchhandlung',
+
+  'Account' => 
+  'Das Konto',
 
   'Parent account' => 
-  'Elterns account',
+  'Geordnete Konto',
+
+  'The parent account does not exist.' => 
+  'Es gibt kein Geordnete Konto',
+
+  'Date' => 
+  'Datum',
 
   'Description' => 
   'Beschreibung',
@@ -39,10 +60,264 @@
   'Description / Explanation' => 
   'Beschreibung / Erklärung',
 
+  'P' => 
+  'P',
+
+  'This account has not been correctly positioned.' => 
+  'Das Konto ist nicht korekt gelegt',
+
+  'According to its definition, the account should not have this kind of outstanding balance.' => 
+  'Nach seiner Definition sollte das Konto nicht über diese Art von ausstehender Saldo verfügen. ***',
+
+  'Create new account' => 
+  'Ein neuer Konto erstellen',
+
+  'Fix chart' => 
+  'Das Fixdiagramm',
+
+  'Create a new account as child of this one' => 
+  'Ein neuer Konto Als Kind von diesem erstellen',
+
+  'Delete this account' => 
+  'eliminiert dieses Konto',
+
+  'New journal entry' => 
+  'Neue Vorlage',
+
+  'Save journal entry' => 
+  'Die Vorlage rettet',
+
+  'This firm does not have any journal entry yet.' => 
+  'Diese Firma hat noch keinen Tagebucheintrag.',
+
+  'Edit account «{name}»' => 
+  'Kontoüberarbeitung «{name}»',
+
+  'Public firms' => 
+  'Öffentliche Firma
+',
+
+  'Your firms' => 
+  'Deine Firma
+',
+
+  'Create firm' => 
+  'Firma herstellen',
+
+  'A firm you know the slug of' => 
+  'Eine Firma derer slug du kennst',
+
+  'Firm not found' => 
+  'Firma nicht gefunden',
+
+  'Templates' => 
+  'Die Vorlagen',
+
+  'Create Template' => 
+  'Vorlage herstellen',
+
+  'Create a Template based on this entry' => 
+  'Nach dieser Buchung Vorlage herstellen',
+
+  'You are going to create a new template with the following accounts:' => 
+  'Man wird eine neue Vorlage mit dem volgem Konto herstellen',
+
+  'Template creation' => 
+  'Vorlageherstellung',
+
+  'The template has been correctly saved.' => 
+  'Die Vorlage ist korrect abgespeichert worden',
+
+  'The template could not be saved.' => 
+  'Die Vorlage konnte nicht abgespeichert sein',
+
+  'A template needs at least two postings.' => 
+  'Eine Vorlage braucht wenigstens zwei Posten',
+
+  'Edit Firm «{name}»' => 
+  'Firma «{name}» verändern',
+
+  'Chart of Accounts' => 
+  'Der Kontenplan',
+
+  'Chart of Accounts and Templates' => 
+  'Kontenplanen und Vorlagen',
+
+  'Chart of Accounts, Templates, and Journal Entries' => 
+  'Kontenplanen, Vorlagen und Buchungen',
+
+  'Delete this firm' => 
+  'Diese Firma ausschalten',
+
+  'Create a Firm' => 
+  'Eine Firma herstellen',
+
+  'Load accounts' => 
+  'Konten Laden',
+
+  'Load all accounts' => 
+  'Laden Sie alle Konten',
+
+  'you cannot do a debit to this kind of account' => 
+  'Man kann nichts auf folgendes Konto aufladen',
+
+  'you cannot do a credit to this kind of account' => 
+  'Man kann nichts auf folgendes Konto beglaubigen',
+
+  'Synchronize accounts' => 
+  'Konten  Synchronisieren',
+
+  'New accounts found' => 
+  'Neue Konten gefunden',
+
+  'Changed accounts found' => 
+  'Geänderte Konten gefunden',
+
+  'There are no accounts to synchronize.' => 
+  'Es sind keine Konten zu synchronisieren.',
+
+  'You can select other languages to have a multilingual chart of accounts.' => 
+  'Sie können andere Sprachen auswählen, um einen mehrsprachigen Kontenplan zu haben',
+
+  'Login with social network account' => 
+  'Anmeldung mit Social Network account',
+
+  'Share the Firm «{name}»' => 
+  'Die Firma «{name}» vertreten',
+
+  'This firm does not seem to have accounts of «{position}» position to close.' => 
+  'Diese Firma scheint keine Konten von «{position}» Position zu schließen.',
+
+  'There must be at least one name for the account.' => 
+  'Es muss mindestens einen Namen für das Konto geben.',
+
+  'Manage one of your firms' => 
+  'Ein von deiner Firma betreiben',
+
+  'Create a new, empty firm' => 
+  'Eine neue leere Firma herstellen  ',
+
+  'View and edit the Chart of Accounts' => 
+  'Anzeigen und Bearbeiten des Kontenplanes',
+
+  'View the Statements' => 
+  'Die Buchhandlung sieht*',
+
+  'Frozen firm' => 
+  'Firma einfrieren',
+
+  'Delete this template' => 
+  'Diese Vorlage abwischen',
+
+  'Are you sure you want to delete this template?' => 
+  'Bist du sicher diese Vorlage abzuwischen',
+
+  'The template has been correctly deleted.' => 
+  'Die Vorlage ist korrect abgewischt worden',
+
+  'The template could not be deleted.' => 
+  'Die Vorlage konnte nicht abgewischt sein',
+
+  'Disown firm' => 
+  'Firma nicht anerkennen',
+
+  'Disown the firm «{name}»' => 
+  'Die Firma «{name}» nicht anerkennen',
+
+  'Statements configuration' => 
+  'Die Buchhandlung darstellt',
+
+  'Choose an account' => 
+  'Wählen Sie ein Konto',
+
+  'The Chart of Accounts is empty.' => 
+  'Der Kontenplan ist leer',
+
+  'Please note that the transaction analysis is experimental and depends on a consistent chart of accounts.' => 
+  'Bitte beachten Sie, dass die Transaktionsanalyse experimentell ist und von einem konsistenten Kontenplan abhängt',
+
+  'Please note that when you create a firm from scratch, it will have an empty chart of accounts, and no configuration at all.' => 
+  'Bitte beachten Sie, dass  wenn Sie eine Firma von Grund auf neu erstellen, wird es einen leeren Kontenplan und keine Konfiguration haben wird.',
+
+  'Toggle the visibility of the selected journal entries in the preparation of the statements' => 
+  'Die Abrechnungs Effekt an die Buchhandlung sichtbar machen,wenn das nicht sind nicht oder umgekehrt',
+
+  'With the selected accounts:' => 
+  'Mit den ausgewählten Konten:',
+
+  'Prepare a journal entry that will close the selected accounts' => 
+  'Bereiten Sie einen Journaleintrag vor, der die ausgewählten Konten schließt',
+
+  'Template update' => 
+  'Vorlage aktualizieren',
+
+  'Import accounts' => 
+  'Konten importieren',
+
+  'Export accounts' => 
+  'Konten exportieren',
+
+  'Place the account here?' => 
+  'Legen Sie das Konto hier?',
+
+  'Use a template' => 
+  'Eine Vorlage benutzen',
+
+  'This template is automatically applied when preparing the statements' => 
+  'Die Vorlage wird beim Buchhandlung Erschaffung automatisch benutzt',
+
+  'This template is not used when preparing the statements' => 
+  'Die Vorlage wird nicht beim Buchhandlung Erschaffung automatisch benutzt',
+
+  'Apply this template automatically.' => 
+  'Diese Vorlage selbsttätig auftragen',
+
+  'Select when you wish this template to be automatically and virtually used each time a financial statement is prepared (without actually seeing the journal entry).' => 
+  'Wenn man diese Vorlage automatisch und virtuell benutzt wird, soll man das benutzen, immer wenn ein Auszug vorbereitet wird.',
+
+  'Here you may configure how the Financial Statement is prepared and the types of accounts available.' => 
+  'Hier können Sie konfigurieren, wie der Jahresabschluss erstellt wird und welche Kontenarten verfügbar sind',
+
+  'Show Chart of Accounts' => 
+  'Zeigen Sie Konten',
+
   'Welcome to «{name}»' => 
   'Willkommen in «{name}»',
 
+  'You are creating an account as a child of «{account}»,' => 
+  'Sie erstellen ein Konto als Kind von «{Konto}»,',
+
+  'You are making the account a child of «{account}»,' => 
+  'Sie machen das Konto ein Kind von «{Konto}»,',
+
+  'The account you are updating is a child of «{account}»,' => 
+  'Das Konto, das Sie aktualisieren, ist ein Kind von «{Konto}»,',
+
+  'the account with code "{code}" is not available (you can add it on the fly to the Chart of Accounts by inserting an exclamation mark at the end of the name, like in "{code}!").' => 
+  'der Konto "{code}" ist nicht verfügbar (Sie können es auf der Fliege zum Diagramm der Konten hinzufügen, indem Sie ein Ausrufezeichen am Ende des Namens einfügen, wie in "{code}!").',
+
   'Fields with <span class="required">*</span> are required.' => 
-  'Felder mit <span class="required">*</ span> sind erforderlich.',
+  'Felder mit <span class="required">*</span> sind erforderlich.',
+
+  'You can synchronize your firm\'s chart of accounts with the one of one of the following ancestors:' => 
+  'Sie können Ihre Firmenplan mit dem von einer der folgenden Vorfahren zu den vorigen synchronisieren:',
+
+  'Edit firm\'s settings' => 
+  'Der Ansatz von die Firma verändern ',
+
+  'This firm is stale' => 
+  'Diese Firma ist ungebraucht',
+
+  'Benchmark Firm' => 
+  'Benchmark Firma',
+
+  'Link current firm' => 
+  'Diese Firma assoziieren',
+
+  'firm\'s description' => 
+  'Beschreibung von der Firma',
+
+  'statements' => 
+  'die Buchhandlung',
 
   );
